390b812fa29df461b21b7b06c13cb99426d2aa5f
Ezequiel Rangel <chekelinho@gmail.com>
2024-10-23 10:43:28 -0700

    Agrega un state de inicio para un nuevo chat

--- src/components/ChatAssistant.tsx
+++ src/components/ChatAssistant.tsx
@@ -1,48 +1,48 @@
 "use client";
 
-import { Send } from "lucide-react";
+import { BotMessageSquare, Send } from "lucide-react";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
-import { ScrollArea, ScrollBar } from "~/components/ui/scroll-area";
 import { useChat } from "ai/react";
 import ChatList from "./ChatList";
-
-const predefinedQuestions = [
-  "Pre-inscripción en el RFC",
-  "Inscripción en el RFC",
-  "Generar contraseña del SAT",
-  "Generar e.firma",
-];
+import { useEffect, useState } from "react";
+import Start from "./Start";
 
 export function ChatAssistant() {
-  const { messages, input, isLoading, handleInputChange, handleSubmit } =
-    useChat({
-      api: "/api/chat",
-    });
+  const [isNewChat, setNewChat] = useState(true);
+  const {
+    messages,
+    input,
+    isLoading,
+    handleInputChange,
+    handleSubmit,
+    append,
+  } = useChat({
+    api: "/api/chat",
+  });
+
+  useEffect(() => {
+    if (isLoading && isNewChat) {
+      setNewChat(false);
+    }
+  }, [isLoading, isNewChat]);
+
+  const handleConversationStarter = async (message: string) => {
+    await append({ id: "1", role: "user", content: message });
+  };
 
   return (
     <div className="w-full h-screen md:h-[calc(100vh-32px)] bg-background border rounded-lg overflow-hidden flex flex-col shadow-lg animate-in slide-in-from-bottom-5 duration-300">
-      <div className="p-4 border-b flex justify-between items-center bg-muted">
-        <h2 className="font-semibold">Chat Assistant</h2>
+      <div className="p-4 border-b flex items-center bg-muted gap-2">
+        <BotMessageSquare />
+        <h2 className="font-semibold">SATBot</h2>
       </div>
-      <ChatList isLoading={isLoading} messages={messages} />
+      {isNewChat ? (
+        <Start onPickOption={handleConversationStarter} />
+      ) : (
+        <ChatList isLoading={isLoading} messages={messages} />
+      )}
       <div className="p-4 border-t">
-        <ScrollArea className="w-full whitespace-nowrap mb-4">
-          <div className="flex space-x-2">
-            {predefinedQuestions.map((question, index) => (
-              <Button
-                key={index}
-                variant="outline"
-                size="sm"
-                className="text-xs py-1 px-2 h-auto"
-                onClick={() => {}}
-              >
-                {question}
-              </Button>
-            ))}
-          </div>
-          <ScrollBar orientation="horizontal" />
-        </ScrollArea>
         <form onSubmit={handleSubmit} className="flex space-x-2">
           <Input
             value={input}
