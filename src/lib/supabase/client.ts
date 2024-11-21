fcfec54000b10796ac1f50842c340388ff320582
Ezequiel Rangel <chekelinho@gmail.com>
2024-11-21 09:54:06 -0700

    Implements Auth with SSR

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,9 +1,9 @@
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
 import { Database } from "~/types/database.types";
 
-const supabase = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
-
-export default supabase;
+export function createClient() {
+  return createBrowserClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+}
